7f778a2fa03ed9f9b3875bae1b3349bf72be166a
maro <0marwandeswign925@gmail.com>
2026-02-09 00:12:56 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function startJourney() {
     alert("هتتنقل لصفحة تانية… اعمل أي حاجة بقى هنا 😎🔥");
 }
-if (password === "2/6") {
+if (password === "9/1") {
     window.location.href = "page2.html";
 }
